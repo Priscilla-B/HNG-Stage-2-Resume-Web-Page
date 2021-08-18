8f56fa430abd5236f7ca78ddf2e36787c4748a6c
Priscilla Baah <pyfbaah@gmail.com>
2021-08-18 16:44:53 +0000

    add script for navigation section

---
+++ resume-page/assets/main.js
@@ -0,0 +1,9 @@
+// nav function
+const navOpener = document.querySelector(".nav-handler");
+const navBar = document.querySelector(".navbar");
+
+navOpener.addEventListener('click', function() {
+  navBar.classList.toggle('open');
+})
+
+
